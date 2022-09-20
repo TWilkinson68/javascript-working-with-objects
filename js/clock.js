af37bfdeb5c85ba157cc93b38965a15d9b2f6e28
Thomas Wilkinson <c2061425@my.shu.ac.uk>
2022-09-20 16:06:50 +0100

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -9,8 +9,46 @@ myNode.innerHTML ="welcome!"
 // console.dir(myDate);
 var updateTime = function(){
     var myDate = new Date ();
-    myNode.innerHTML = myDate.toTimeString();
+    myNode.innerHTML = myDate.toLocaleTimeString();
 }
 setInterval(updateTime, 1000);
+
+
+myNode.innerHTML = myDate.toTimeString().substr(0, 8);
+upDateTime();
+setInterval(updateTime, 1000);
+
+
+// var myDate = new Date();
+// var myHours = myDate.getHours();
+// var myMins = myDate.getMinutes();
+// var mySeconds = myDate.getSeconds();
+// var myTimeDisplay = myHours + ":"+ myMins +":"+ mySeconds;
+// myNode.innerHTML = myTimeDisplay;
+
+// if(myHours < 10){
+//     myHours = "0"+ myHours;
+//     }
+//     if(myMins < 10){
+//     myMins = "0"+myMins;
+//     }
+//     if(mySeconds < 10){
+//     mySeconds = "0"+mySeconds;}
+
+//     var checkForLeadingZeros = function (timeVal){
+//         if(timeVal < 10){
+//         timeVal = "0"+timeVal;
+//         }
+//         return timeVal;}
+
+//         var myDate = new Date();
+// var myHours = checkForLeadingZeros(myDate.getHours());
+// var myMins = checkForLeadingZeros(myDate.getMinutes());
+// var mySeconds = checkForLeadingZeros(myDate.getSeconds());
+
+
+// myNode.innerHTML = myDate.ToTimeString().substr(0, 8);
+// upDateTime();
+// setInterval(UpDateTime, 1000);
 
 
