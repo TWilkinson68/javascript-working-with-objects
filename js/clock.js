c59a4137daed96c3f01c100c1a1d01bccfc81202
Thomas Wilkinson <c2061425@my.shu.ac.uk>
2022-09-21 10:43:58 +0100

    changes to clock.js and staff list

--- js/clock.js
+++ js/clock.js
@@ -9,10 +9,40 @@ myNode.innerHTML ="welcome!"
 var updateTime = function(){
     var myDate = new Date ();
     myNode.innerHTML = myDate.toTimeString(). substring(0,8);
-}
+    let seconds = myDate.getSeconds();
+    let minutes = myDate.getMinutes();
+    let hours = myDate.getHours();
+    hours = hours + minutes/60;
+    let rotSeconds = (seconds * 6) - 90;
+    let rotMinutes = (minutes * 6) - 90;
+    let rotHours = (hours * 30) - 90;
+    document.getElementById("secondHand").style.transform = `rotate(${rotSeconds}deg)`;
+    document.getElementById('minHand').style.transform = `rotate(${rotMinutes}deg)`;
+    document.getElementById('hrHand').style.transform = `rotate(${rotHours}deg)`;
+}    
 setInterval(updateTime, 1000);
 updateTime();
 
+
+document.getElementById('hrHand').style.backgroundColor = '#0f0';
+
+
+
+
+
+S
+// document.getElementById("secondHand").style.transform = 'rotate('+rotSeconds+'deg)';
+
+
+
+
+
+
+//     "editor.defaultFormatter"; "esbenp.prettier-vscode",
+//     "[javascript]"; {
+//       "editor.defaultFormatter"; "esbenp.prettier-vscode"
+//     }
+//   }
 
 // myNode.innerHTML = myDate.toTimeString().substr(0, 8);
 // upDateTime();
@@ -50,5 +80,16 @@ updateTime();
 // myNode.innerHTML = myDate.ToTimeString().substr(0, 8);
 // upDateTime();
 // setInterval(UpDateTime, 1000);
+
+
+// document.getElementById('hrHand').style.backgroundColor = '#0f0';
+// var rotSeconds = (seconds * 6) - 90;
+
+
+// document.getElementById('secondHand').style.transform =
+// 'rotate('+rotSeconds+'deg)'; 
+
+// document.getElementById('minHand')
+// var rotMinutes(seconds * 
 
 
